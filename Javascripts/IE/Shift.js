67ceeb821cf7bab97fb8955af9a2efa4afd4762f
Philip Ross <philip.ross@flowtrac.com>
2007-10-03 18:46:38 +0000

    Optimize Shifts
    Optimize Shifts, and fixing IE shift bug.

--- Javascripts/IE/Shift.js
+++ Javascripts/IE/Shift.js
@@ -1,6 +1,6 @@
 function ShiftStart(objArray)
 {
-	var xPos, yPos, Obj;
+	var xPos, yPos, obj;
 	thisObjArray = objArray;
 	thisObjArray.Ghosts = Array();
 	thisObjArray.HasMoved = false;
@@ -10,41 +10,57 @@ function ShiftStart(objArray)
 
 	thisObjArray.CursorStartX = xPos;
 	thisObjArray.CursorStartY = yPos;
-
-	for(var i=0; i<thisObjArray.length; i++)
+	var tmpCount = thisObjArray.length;
+	for(var i=0; i<tmpCount; ++i)
 	{
 		if(thisObjArray[i][2] == 1)
 		{
-			Obj = document.getElementById(thisObjArray[i][0]).cloneNode(true);
-			Obj.style.position = "absolute";
-			Obj.style.left = FindX(thisObjArray[i][0]) + "px";
-			Obj.style.top = FindY(thisObjArray[i][0]) + "px";
-			Obj.style.filter = "alpha(opacity=50)";
-			thisObjArray[i][0] = Obj.id = thisObjArray[i][0] + "_Ghost";
-			document.body.appendChild(Obj);
+			obj = document.getElementById(thisObjArray[i][0]).cloneNode(true);
+			obj.style.position = "absolute";
+			obj.style.left = FindX(thisObjArray[i][0]) + "px";
+			obj.style.top = FindY(thisObjArray[i][0]) + "px";
+			obj.style.filter = "alpha(opacity=50)";
+			thisObjArray[i][0] = obj.id = thisObjArray[i][0] + "_Ghost";
+			document.body.appendChild(obj);
 			thisObjArray.Ghosts[thisObjArray.Ghosts.length] = i;
 		}
 		else
-			Obj = document.getElementById(thisObjArray[i][0]);
-		thisObjArray[i].StartWidth = parseInt(Obj.style.width, 10);
-		thisObjArray[i].StartHeight = parseInt(Obj.style.height,  10);
-		thisObjArray[i].StartLeft = parseInt(Obj.style.left, 10);
-		thisObjArray[i].StartTop = parseInt(Obj.style.top,  10);
-		if(isNaN(thisObjArray[i].StartHeight))
-			thisObjArray[i].StartHeight = 20;
-		if(isNaN(thisObjArray[i].StartTop))
-			thisObjArray[i].StartTop = yPos;
-		//Combine Above with SetShiftWithInitials - Asher
-		if(Obj.ShiftsWith != null)
-			SetShiftWithInitials(Obj.ShiftsWith);
-		BringToFront(thisObjArray[i][0]);
+			obj = document.getElementById(thisObjArray[i][0]);
+		_NShiftInitObject(thisObjArray[i], obj);
+		SetShiftWithInitials(obj);
+//		BringToFront(thisObjArray[i][0]);
+		BringToFront(obj.id);
 	}
 	document.attachEvent("onmousemove", ShiftObj);
 	document.attachEvent("onmouseup",   ShiftStop);
 	window.event.cancelBubble = true;
 	window.event.returnValue = false;
 }
-
+function _NShiftInitObject(info, obj)
+{
+	info.StartWidth = obj.offsetWidth;
+	info.StartHeight = obj.offsetHeight;
+	info.StartLeft = obj.offsetLeft;
+	info.StartTop = obj.offsetTop;
+	if(isNaN(info.StartHeight))
+		info.StartHeight = 20;
+	if(isNaN(info.StartTop))
+		info.StartTop = yPos;
+}
+function SetShiftWithInitials(obj)
+{	
+	if(obj.ShiftsWith != null)
+	{
+		var tmpCount = obj.ShiftsWith.length;
+		var subObject;
+		for(var j=0; j<tmpCount; ++j)
+		{
+			subObject = document.getElementById(obj.ShiftsWith[j][0]);
+			_NShiftInitObject(obj.ShiftsWith[j], subObject);
+			SetShiftWithInitials(subObject);
+		}
+	}
+}
 function ShiftObj()
 {
 	var xPos, yPos;
@@ -62,115 +78,50 @@ function ShiftObj()
 }
 function ShiftObjects(objects, deltaX, deltaY)
 {
-	var tempFinalHor;
-	var tempFinalVer;
-
-	for(var i=0; i<objects.length; i++)
+	var tmpCount = objects.length;
+	for(var i=0; i<tmpCount; ++i)
 	{
-		if(objects[i][1] == 1 || objects[i][1] == 3)
+		if(objects[i][1] <= 3)
 		{
-			tempFinalHor = Math.round(objects[i].StartWidth + deltaX * objects[i][7]);
-			
-			if(objects[i][3] != null && objects[i][4] != null)
-			{
-				if(tempFinalHor >= objects[i][3] && tempFinalHor <= objects[i][4])
-					ChangeAndSave(objects[i][0], "style.width", tempFinalHor  + "px");
-				else 
-					ChangeAndSave(objects[i][0], "style.width", (tempFinalHor >= objects[i][3]) ? objects[i][4] + "px" : objects[i][3] + "px");
-			}
-			else if(objects[i][3] != null)
-				ChangeAndSave(objects[i][0], "style.width", (tempFinalHor >= objects[i][3]) ? tempFinalHor  + "px" : objects[i][3] + "px");
-			else 
-				ChangeAndSave(objects[i][0], "style.width",(tempFinalHor <= objects[i][4]) ? tempFinalHor  + "px" : objects[i][4] + "px");
+			if(objects[i][1] != 1)
+				ShiftObject(objects[i], "style.height", objects[i].StartHeight, deltaY , objects[i][5], objects[i][6]);
+			if(objects[i][1] != 2)	
+				ShiftObject(objects[i], "style.width", objects[i].StartWidth, deltaX , objects[i][3], objects[i][4]);
 		}
-		if(objects[i][1] == 2 || objects[i][1] == 3)
+		else
 		{
-			tempFinalVer = Math.round(objects[i].StartHeight + deltaY * objects[i][7]);
-			
-			if(objects[i][5] != null && objects[i][6] != null)
-			{
-				if(tempFinalVer >= objects[i][5] && tempFinalVer <= objects[i][6])
-					ChangeAndSave(objects[i][0], "style.height", tempFinalVer+"px");
-				else 
-					ChangeAndSave(objects[i][0], "style.height", (tempFinalVer >= objects[i][5]) ? objects[i][6] + "px" : objects[i][5] + "px");
-			}
-			else if(objects[i][5] != null)
-				ChangeAndSave(objects[i][0], "style.height", (tempFinalVer >= objects[i][5]) ? tempFinalVer  + "px" : objects[i][5] + "px");
-			else
-				ChangeAndSave(objects[i][0], "style.height", (tempFinalVer <= objects[i][6]) ? tempFinalVer  + "px" : objects[i][6] + "px");		
-		}
-		if(objects[i][1] == 4 || objects[i][1] == 6)
-		{
-			tempFinalHor = Math.round(objects[i].StartLeft + (deltaX * objects[i][7]));
-			if(objects[i][3] != null && objects[i][4] != null)
-			{
-				if(tempFinalHor >= objects[i][3] && tempFinalHor <= objects[i][4])
-					ChangeAndSave(objects[i][0], "style.left", tempFinalHor + "px");
-				else 
-					ChangeAndSave(objects[i][0], "style.left", (tempFinalHor >= objects[i][3]) ? objects[i][4] + "px" : objects[i][3] + "px");
-			}
-			else if(objects[i][3] == null && objects[i][4] == null)
-				ChangeAndSave(objects[i][0], "style.left", tempFinalHor + "px");
-			else if(objects[i][3] != null)
-				ChangeAndSave(objects[i][0], "style.left", (tempFinalHor >= objects[i][3]) ? tempFinalHor  + "px" : objects[i][3] + "px");
-			else 
-				ChangeAndSave(objects[i][0], "style.left", (tempFinalHor <= objects[i][4]) ? tempFinalHor  + "px" : objects[i][4] + "px");					
-		}
-		if(objects[i][1] == 5 || objects[i][1] == 6)
-		{
-			tempFinalVer = Math.round(objects[i].StartTop + deltaY * objects[i][7]);
-			
-			if(objects[i][5] != null && objects[i][6] != null)
-			{
-				if(tempFinalVer >= objects[i][5] && tempFinalVer <= objects[i][6])
-					ChangeAndSave(objects[i][0], "style.top", tempFinalVer + "px");
-				else 
-					ChangeAndSave(objects[i][0], "style.top", (tempFinalVer >= objects[i][5]) ? objects[i][6] + "px" : objects[i][5] + "px");
-			}
-			else if(objects[i][5] == null && objects[i][6] == null)
-				ChangeAndSave(objects[i][0], "style.top", tempFinalVer  + "px");
-			else if(objects[i][5] != null)
-				ChangeAndSave(objects[i][0], "style.top", (tempFinalVer >= objects[i][5]) ? tempFinalVer  + "px" : objects[i][5] + "px");
-			else 
-				ChangeAndSave(objects[i][0], "style.top", (tempFinalVer <= objects[i][6]) ? tempFinalVer  + "px" : objects[i][6] + "px");
+			if(objects[i][1] != 4)
+				ShiftObject(objects[i], "style.top", objects[i].StartTop, deltaY , objects[i][5], objects[i][6]);
+			if(objects[i][1] != 5)
+				ShiftObject(objects[i], "style.left", objects[i].StartLeft, deltaX , objects[i][3], objects[i][4]);
 		}
 		var tmpObj = document.getElementById(objects[i][0]);
 		if(tmpObj.ShiftsWith != null)
-			ShiftObjects(tmpObj.ShiftsWith, deltaX, deltaY);     
+			ShiftObjects(tmpObj.ShiftsWith, deltaX, deltaY);
 	}
 }
-function SetShiftWithInitials(objects)
+function ShiftObject(object, property, start, delta, minBound, maxBound)
 {
-	for(var j=0; j<objects.length; j++)
-	{
-		var Obj = document.getElementById(objects[j][0]);
-		objects[j].StartWidth = parseInt(Obj.style.width, 10);
-		objects[j].StartHeight = parseInt(Obj.style.height,  10);
-		objects[j].StartLeft = parseInt(Obj.style.left, 10);
-		objects[j].StartTop = parseInt(Obj.style.top,  10);
-		if(isNaN(objects[j][j].StartHeight))
-			objects[j].StartHeight = 20;
-		if(isNaN(objects[j].StartTop))
-			objects[j].StartTop = yPos;
-		if(Obj.ShiftsWith != null)
-			SetShiftWithInitials(Obj.ShiftsWith);
-	}
+	var finalCoord = Math.round(start + delta * object[7]);
+	ChangeAndSave(object[0], property, (minBound != null && finalCoord <= minBound ? minBound : (maxBound != null && finalCoord >= maxBound ? maxBound : finalCoord))+"px");	
 }
 function ShiftStop()
 {
+	var tmpCount;
 	if(thisObjArray.HasMoved)
 	{
 		var Catcher, CatcherLeft, CatcherTop, DroppedX, DroppedY, j;
 		DroppedX = window.event.clientX + document.documentElement.scrollLeft + document.body.scrollLeft;
 		DroppedY = window.event.clientY + document.documentElement.scrollTop + document.body.scrollTop;
-		for(var i=0; i<NOLOHCatchers.length; i++)
+		tmpCount = NOLOHCatchers.length;
+		for(var i=0; i<tmpCount; ++i)
 			if(IsAvailable(NOLOHCatchers[i]))
 			{
 				Catcher = document.getElementById(NOLOHCatchers[i]);
 				CatcherX = FindX(NOLOHCatchers[i]);
 				CatcherY = FindY(NOLOHCatchers[i]);
 				if(DroppedX >= CatcherX && DroppedX < CatcherX + (Catcher.style.width==""?80:parseInt(Catcher.style.width,10)) && DroppedY >= CatcherY && DroppedY < CatcherY + (Catcher.style.height==""?20:parseInt(Catcher.style.height,10)))
-					for(j=0; j<thisObjArray.length; j++)
+					for(j=0; j<thisObjArray.length; ++j)
 						if(4 <= thisObjArray[j][1] && thisObjArray[j][1] <= 6 && NOLOHCatchers[i]!=thisObjArray[j][0].replace("_Ghost",""))
 							NOLOHCaught.push(thisObjArray[j][0].replace("_Ghost",""));
 				if(NOLOHCaught.length != 0)
@@ -180,7 +131,8 @@ function ShiftStop()
 				}
 			}
 	}
-	for(i=0; i<thisObjArray.Ghosts.length; i++)
+	tmpCount = thisObjArray.Ghosts.length;
+	for(i=0; i<tmpCount; ++i)
 	{
 		j = thisObjArray.Ghosts[i];
 		document.body.removeChild(document.getElementById(thisObjArray[j][0]));
@@ -189,7 +141,8 @@ function ShiftStop()
 	if(!thisObjArray.HasMoved)
 	{
 		var obj;
-		for(i=0; i<thisObjArray.length; i++)
+		tmpCount = thisObjArray.length;
+		for(i=0; i<tmpCount; ++i)
 		{
 			obj = document.getElementById(thisObjArray[i][0]);
 			if(obj.onclick != null)
