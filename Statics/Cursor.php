4728af789f6c0ae438292fca66024f9c88c06e51
Philip Ross <philip.ross@flowtrac.com>
2007-11-05 01:56:36 +0000

    Statics Cleanup Part 1

--- Statics/Cursor.php
+++ Statics/Cursor.php
@@ -4,21 +4,21 @@
  */
 final class Cursor
 {
-	const Arrow = "default";
-	const Crosshair = "crosshair";
-	const Hand = "pointer";
-	const EastResize = "e-resize";
-	const Help = "help";
-	const Move = "move";
-	const NorthResize = "n-resize";
-	const NorthEastResize = "ne-resize";
-	const NorthWestResize = "nw-resize";
-	const SouthResize = "s-resize";
-	const SouthEastResize = "se-resize";
-	const SouthWestResize = "sw-resize";
-	const Text = "text";
-	const WestResize = "w-resize";
-	const Wait = "wait";
+	const Arrow = 'default';
+	const Crosshair = 'crosshair';
+	const Hand = 'pointer';
+	const EastResize = 'e-resize';
+	const Help = 'help';
+	const Move = 'move';
+	const NorthResize = 'n-resize';
+	const NorthEastResize = 'ne-resize';
+	const NorthWestResize = 'nw-resize';
+	const SouthResize = 's-resize';
+	const SouthEastResize = 'se-resize';
+	const SouthWestResize = 'sw-resize';
+	const Text = 'text';
+	const WestResize = 'w-resize';
+	const Wait = 'wait';
 	
 	private function Cursor(){}
 	
---
+++ Statics/Layout.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Statics
+ */
+class Layout
+{
+	const Absolute = 0;
+	const Relative = 1;
+	const Web = 2;
+	const Left = 'left';
+	const Top = 'top';
+	const Bottom = 'bottom';
+	const Right = 'right';
+}
+?>
