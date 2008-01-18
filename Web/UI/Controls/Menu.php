50558b5fb6d998b7361151dd63ba7a026f110d7f
Philip Ross <philip.ross@flowtrac.com>
2008-01-18 16:37:59 +0000

    Menu Rename2

---
+++ Web/UI/Controls/Menu.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package Web.UI.Controls
+ */
+class Menu extends Panel
+{
+	public $MenuItems;
+	public $MenuType = "Click";
+	
+	function Menu($left = 0, $top = 0, $width = 0)
+	{
+		parent::Panel($left, $top, $width, 18, $this);
+		$this->BackColor = "#f1f1ed";
+		$this->MenuItems = &$this->Controls;
+		$this->MenuItems->AddFunctionName = "AddMenuItem";
+		$this->Scrolling = System::Full;
+	}
+	function AddMenuItem(MenuItem $menuItem)
+	{
+		$tempCount = $this->MenuItems->Count();
+		if($tempCount > 0)
+			$menuItem->SetLeft($this->MenuItems->Item[$tempCount -1]->GetRight());
+		else
+			$menuItem->SetLeft(0);
+		$menuItem->MainMenuPanel->SetLeft(0);//$menuItem->GetLeft());
+		$menuItem->MainMenuPanel->SetTop($menuItem->GetBottom());//GetTop() + $menuItem->GetHeight());
+		if($this->MenuType == 'Click')
+		{
+			$menuItem->Click = new ClientEvent("ToggleSubMenuItems('{$menuItem->Id}','{$menuItem->TextLabel->Id}','{$menuItem->MainMenuPanel->Id}', true);");
+			//$menuItem->MouseOver = new ClientEvent("ChangeMenuOutColors('{$menuItem->TextLabel->Id}','#316AC5', '#FFFFFF');");
+			//$menuItem->MouseOver[] = new ClientEvent("ToggleSubMenuItems('{$menuItem->Id}', '{$menuItem->TextLabel->Id}','{$menuItem->MainMenuPanel->Id}', false);");
+			//$menuItem->MouseOut = new ClientEvent("ChangeMenuOutColors('{$menuItem->TextLabel->Id}','{$menuItem->OutBackColor}', '{$menuItem->OutTextColor}');");
+		}
+		$this->MenuItems->Add($menuItem, true, true);
+		NolohInternal::SetProperty("IsMainMenu","true", $menuItem->TextLabel);
+		NolohInternal::SetProperty("MenuPanelParentId", $this->Id, $menuItem->TextLabel);
+		//QueueClientFunction($this, "document.getElementById('{$menuItem->TextLabel->Id}').IsMainMenu = true;");
+		//AddScript("document.getElementById('{$menuItem->TextLabel->Id}').IsMainMenu = true; document.getElementById('{$menuItem->TextLabel->Id}').MenuPanelParentId = '{$this->Id}';");
+	}
+	function Clear()
+	{
+		$this->Controls->Clear();
+	}
+}
+?>
