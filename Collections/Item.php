44d0c77c0ce25115928953c3cc82613b69759e31
Philip Ross <philip.ross@flowtrac.com>
2008-06-09 11:47:17 +0000

    Item to extend Object

--- Collections/Item.php
+++ Collections/Item.php
@@ -1,17 +1,30 @@
 <?php
 /**
- * @package Collections
- */
-/**
  * Item class
  * 
- * We're sorry, but this class doesn't have a description yet. We're working very hard on our documentation so check back soon!
+ * An Item is an Object for holding a Text and Value pair. These are most commonly used in connection with ListControl but could serve for anything requiring a Text and Value pair.
+ * One important thing to note about Item is that it will not in any way ping its parent collection when its Text or Value changes. Instead, a new Item should be created.
+ * 
+ * @package Collections
  */
-class Item
+class Item extends Object
 {
+	/**
+	 * The Item's Text
+	 * @var mixed
+	 */
 	public $Text;
+	/**
+	 * The Item's Value
+	 * @var mixed
+	 */
 	public $Value;
-	
+	/**
+	 * Constructor. Be sure to call this from the constructor of any class that extends Item.
+	 * @param mixed $text
+	 * @param mixed $value
+	 * @return Item
+	 */
 	function Item($text = '', $value = '')
 	{
 		$this->Text = $text;
