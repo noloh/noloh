37dc656419e5b34d9fb3fb1253780664e3a9ec55
Philip Ross <philip.ross@flowtrac.com>
2008-07-30 06:53:45 +0000

    Animate anything to Oblivion

--- Statics/Animate.php
+++ Statics/Animate.php
@@ -11,14 +11,19 @@ final class Animate
 	const Linear = 1;
 	const Quadratic = 2;
 	const Cubic = 3;
-	const Oblivion = 'Oblivion';
+	const Oblivion = null;
 	
 	private function Animate() {}
 	
 	static function Property($control, $property, $from, $to, $duration=1000, $units=null, $easing=Animate::Quadratic, $fps=30)
 	{
 		AddNolohScriptSrc('Animation.js', true);
-		QueueClientFunction($control, '_NAniStart', array('\''.$control->Id.'\'', '\''.$property.'\'', $from!==null?$from:0, $to, $duration, '\''.$units.'\'', $easing, $fps), false, Priority::Low);
+		if($to === null)
+		{
+			$to = 1;
+			NolohInternal::SetProperty('_NOblivionC', true, $control);
+		}
+		QueueClientFunction($control, '_NAniStart', array('\''.$control->Id.'\'', '\''.$property.'\'', $from!==null?$from:('_N("'.$control->Id.'").'.$property), $to, $duration, '\''.$units.'\'', $easing, $fps), false, Priority::Low);
 	}
 	
 	static function Left($control, $to, $duration=1000, $easing=Animate::Quadratic, $from=null, $fps=30)
@@ -43,13 +48,11 @@ final class Animate
 	
 	static function Width($control, $to, $duration=1000, $easing=Animate::Quadratic, $from=null, $fps=30)
 	{
-		self::ProcessOblivion($control, $to);
 		Animate::Property($control, 'style.width', $from===null?$control->Width:$from, $to, $duration, 'px', $easing, $fps);
 	}
 	
 	static function Height($control, $to, $duration=1000, $easing=Animate::Quadratic, $from=null, $fps=30)
 	{
-		self::ProcessOblivion($control, $to);
 		Animate::Property($control, 'style.height', $from===null?$control->Height:$from, $to, $duration, 'px', $easing, $fps);
 	}
 	
@@ -64,7 +67,7 @@ final class Animate
 		if($to instanceof Control)
 			$to = '_N(\''. $to->Id .'\').offsetLeft';
 		$from = $from===null?$control->ScrollLeft: $from==Layout::Left?0: $from==Layout::Right?9999: $from;
-		$to = $to==Layout::Left?0: $to==Layout::Right?9999: $to;
+		$to = $to==Layout::Left?0: $to==Layout::Right?('_N("'.$control->Id.'").scrollWidth'): $to;
 		Animate::Property($control, 'scrollLeft', $from, $to, $duration, '', $easing, $fps);
 	}
 	
@@ -73,25 +76,13 @@ final class Animate
 		if($to instanceof Control)
 			$to = '_N(\''. $to->Id .'\').offsetTop';
 		$from = $from===null?$control->ScrollTop: $from==Layout::Top?0: $from==Layout::Bottom?9999: $from;
-		$to = $to==Layout::Top?0: $to==Layout::Bottom?9999: $to;
+		$to = $to==Layout::Top?0: $to==Layout::Bottom?('_N("'.$control->Id.'").scrollHeight'): $to;
 		Animate::Property($control, 'scrollTop', $from, $to, $duration, '', $easing, $fps);
 	}
 	
 	static function Opacity($control, $to, $duration=1000, $easing=Animate::Quadratic, $from=null, $fps=30)
 	{
-		self::ProcessOblivion($control, $to);
 		Animate::Property($control, 'opacity', $from===null?$control->Opacity:$from, $to, $duration, '', $easing, $fps);
-	}
-	/**
-	 * @ignore
-	 */
-	static function ProcessOblivion($control, &$to)
-	{
-		if($to === 'Oblivion')
-		{
-			$to = 1;
-			NolohInternal::SetProperty('_NOblivionC', true, $control);
-		}
 	}
 }
 
