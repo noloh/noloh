c020da4817a5251810c3fd2132df99dbedda1b38
Philip Ross <philip.ross@flowtrac.com>
2008-09-07 04:13:18 +0000

    Mac Safari Crash
    Cookie expiration GMT instead of UTC

--- System/WebPage.php
+++ System/WebPage.php
@@ -327,7 +327,7 @@ UserAgent::IsIE() ? '
 
 <SCRIPT type="text/javascript">
   _NApp = ', $GLOBALS['_NApp'], ';
-  document.cookie = "NAppCookie=; expires=Thu, 1 Jan 1970, 00:00:00 UTC; path=/";', 
+  document.cookie = "NAppCookie=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/";', 
 $_SESSION['_NIE6'] ? '
   function _NIe6InitIframeLoad()
   {
