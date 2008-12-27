5efc8a07b78d395aff29fc0cf54a53809c3ae666
Philip Ross <philip.ross@flowtrac.com>
2008-12-27 18:40:19 +0000

    Autoload

--- Includes.php
+++ Includes.php
@@ -1,119 +1,121 @@
 <?php
 
-// Root
-// **********************************************************************
+// Always include
 require($_NPath . 'NOLOHConfig.php');
-
-// System
-// **********************************************************************
-require($_NPath . 'System/Object.php');
-require($_NPath . 'System/Application.php');
-require($_NPath . 'System/Component.php');
-require($_NPath . 'System/GeneralFunctions.php');
-require($_NPath . 'System/Multiple.php');
-require($_NPath . 'System/NolohInternal.php');
-require($_NPath . 'System/Pointer.php');
-require($_NPath . 'System/WebPage.php');
-
-// Collections
-// **********************************************************************
-require($_NPath . 'Collections/ArrayList.php');
-require($_NPath . 'Collections/Container.php');
-require($_NPath . 'Collections/ControlPair.php');
-require($_NPath . 'Collections/Group.php');
-require($_NPath . 'Collections/ImplicitArrayList.php');
-require($_NPath . 'Collections/Item.php');
-
-// Data
-// **********************************************************************
-require($_NPath . 'Data/Data.php');
-require($_NPath . 'Data/DataConnection.php');
-require($_NPath . 'Data/DataCommand.php');
-require($_NPath . 'Data/DataConstraint.php');
-require($_NPath . 'Data/DataReader.php');
-require($_NPath . 'Data/File.php');
-
-// Events
-// **********************************************************************
-require($_NPath . 'Events/Event.php');
-require($_NPath . 'Events/ClientEvent.php');
-require($_NPath . 'Events/ServerEvent.php');
-
-// Interfaces
-// **********************************************************************
 require($_NPath . 'Interfaces/Groupable.php');
 require($_NPath . 'Interfaces/MultiGroupable.php');
 require($_NPath . 'Interfaces/Singleton.php');
-
-// Core Controls
-// **********************************************************************
+require($_NPath . 'System/Object.php');
+require($_NPath . 'System/Application.php');
+require($_NPath . 'System/Component.php');
+require($_NPath . 'System/Error.php');
+require($_NPath . 'System/GeneralFunctions.php');
+require($_NPath . 'System/NolohInternal.php');
+require($_NPath . 'System/Pointer.php');
+require($_NPath . 'System/WebPage.php');
+require($_NPath . 'Collections/ArrayList.php');
 require($_NPath . 'Controls/Core/Control.php');
-require($_NPath . 'Controls/Core/Button.php');
-require($_NPath . 'Controls/Core/Panel.php');
-require($_NPath . 'Controls/Core/Calendar.php');
-require($_NPath . 'Controls/Core/CheckControl.php');
-require($_NPath . 'Controls/Core/CheckBox.php');
-require($_NPath . 'Controls/Core/ListControl.php');
-require($_NPath . 'Controls/Core/CheckListBox.php');
-require($_NPath . 'Controls/Core/ComboBox.php');
-require($_NPath . 'Controls/Core/FileUpload.php');
-require($_NPath . 'Controls/Core/Form.php');
-require($_NPath . 'Controls/Core/IFrame.php');
-require($_NPath . 'Controls/Core/Image.php');
-require($_NPath . 'Controls/Core/Label.php');
-require($_NPath . 'Controls/Core/Link.php');
-require($_NPath . 'Controls/Core/ListBox.php');
-require($_NPath . 'Controls/Core/MarkupRegion.php');
-require($_NPath . 'Controls/Core/Multimedia.php');
-require($_NPath . 'Controls/Core/RadioButton.php');
-require($_NPath . 'Controls/Core/Table.php');
-require($_NPath . 'Controls/Core/TextArea.php');
-require($_NPath . 'Controls/Core/TextBox.php');
-require($_NPath . 'Controls/Core/Timer.php');
-
-// Extended Controls
-// **********************************************************************
-require($_NPath . 'Controls/Extended/Accordion.php');
-require($_NPath . 'Controls/Extended/ListView.php');
-require($_NPath . 'Controls/Extended/CheckListView.php');
-require($_NPath . 'Controls/Extended/CollapsePanel.php');
-require($_NPath . 'Controls/Extended/Menu.php');
-require($_NPath . 'Controls/Extended/ContextMenu.php');
-require($_NPath . 'Controls/Extended/DatePicker.php');
-require($_NPath . 'Controls/Extended/RichMarkupRegion.php');
-require($_NPath . 'Controls/Extended/RolloverImage.php');
-require($_NPath . 'Controls/Extended/RolloverLabel.php');
-require($_NPath . 'Controls/Extended/RolloverTab.php');
-require($_NPath . 'Controls/Extended/TabPanel.php');
-require($_NPath . 'Controls/Extended/TransferPanel.php');
-require($_NPath . 'Controls/Extended/TreeList.php');
-require($_NPath . 'Controls/Extended/WindowPanel.php');
-
-// Auxilary Controls
-// **********************************************************************
-require($_NPath . 'Controls/Auxilary/AccordionPart.php');
-require($_NPath . 'Controls/Auxilary/ColumnHeader.php');
-require($_NPath . 'Controls/Auxilary/MarkupItem.php');
-require($_NPath . 'Controls/Auxilary/Eventee.php');
-require($_NPath . 'Controls/Auxilary/Larva.php');
-require($_NPath . 'Controls/Auxilary/ListViewItem.php');
-require($_NPath . 'Controls/Auxilary/MenuItem.php');
-require($_NPath . 'Controls/Auxilary/Tab.php');
-require($_NPath . 'Controls/Auxilary/TableColumn.php');
-require($_NPath . 'Controls/Auxilary/TableRow.php');
-require($_NPath . 'Controls/Auxilary/TabPage.php');
-require($_NPath . 'Controls/Auxilary/TreeNode.php');
-
-// Statics
-// **********************************************************************
-require($_NPath . 'Statics/Animate.php');
-require($_NPath . 'Statics/ClientScript.php');
-require($_NPath . 'Statics/Cursor.php');
-require($_NPath . 'Statics/Layout.php');
+require($_NPath . 'Events/Event.php');
+require($_NPath . 'Events/ClientEvent.php');
+require($_NPath . 'Events/ServerEvent.php');
 require($_NPath . 'Statics/Priority.php');
-require($_NPath . 'Statics/Shift.php');
 require($_NPath . 'Statics/System.php');
 require($_NPath . 'Statics/URL.php');
 require($_NPath . 'Statics/UserAgent.php');
+
+function _NAutoLoad($class)
+{
+	global $_NAutoLoad, $_NPath;
+	if(!isset($_NAutoLoad))
+		$_NAutoLoad = array(
+			
+			// System
+			'Multiple' => 			'System/Multiple.php',
+			
+			// Collections
+			'Container' => 			'Collections/Container.php',
+			'ControlPair' => 		'Collections/ControlPair.php',
+			'Group' => 				'Collections/Group.php',
+			'ImplicitArrayList' => 	'Collections/ImplicitArrayList.php',
+			'Item' => 				'Collections/Item.php',
+			
+			// Data
+			'Data' => 				'Data/Data.php',
+			'DataConnection' => 	'Data/DataConnection.php',
+			'DataCommand' => 		'Data/DataCommand.php',
+			'DataConstraint' => 	'Data/DataConstraint.php',
+			'DataReader' => 		'Data/DataReader.php',
+			'File' => 				'Data/File.php',
+			
+			// Core Controls
+			'Button' => 			'Controls/Core/Button.php',
+			'Panel' => 				'Controls/Core/Panel.php',
+			'Calendar' => 			'Controls/Core/Calendar.php',
+			'CheckControl' => 		'Controls/Core/CheckControl.php',
+			'CheckBox' => 			'Controls/Core/CheckBox.php',
+			'ListControl' => 		'Controls/Core/ListControl.php',
+			'CheckListBox' => 		'Controls/Core/CheckListBox.php',
+			'ComboBox' => 			'Controls/Core/ComboBox.php',
+			'FileUpload' => 		'Controls/Core/FileUpload.php',
+			'Form' => 				'Controls/Core/Form.php',
+			'IFrame' => 			'Controls/Core/IFrame.php',
+			'Image' => 				'Controls/Core/Image.php',
+			'Label' => 				'Controls/Core/Label.php',
+			'Link' => 				'Controls/Core/Link.php',
+			'ListBox' => 			'Controls/Core/ListBox.php',
+			'MarkupRegion' => 		'Controls/Core/MarkupRegion.php',
+			'Multimedia' => 		'Controls/Core/Multimedia.php',
+			'RadioButton' => 		'Controls/Core/RadioButton.php',
+			'Table' => 				'Controls/Core/Table.php',
+			'TextArea' => 			'Controls/Core/TextArea.php',
+			'TextBox' => 			'Controls/Core/TextBox.php',
+			'Timer' => 				'Controls/Core/Timer.php',
+			
+			// Extended Controls
+			'Accordion' => 			'Controls/Extended/Accordion.php',
+			'ListView' => 			'Controls/Extended/ListView.php',
+			'CheckListView' => 		'Controls/Extended/CheckListView.php',
+			'CollapsePanel' => 		'Controls/Extended/CollapsePanel.php',
+			'Menu' => 				'Controls/Extended/Menu.php',
+			'ContextMenu' => 		'Controls/Extended/ContextMenu.php',
+			'DatePicker' => 		'Controls/Extended/DatePicker.php',
+			'RichMarkupRegion' => 	'Controls/Extended/RichMarkupRegion.php',
+			'RolloverImage' => 		'Controls/Extended/RolloverImage.php',
+			'RolloverLabel' => 		'Controls/Extended/RolloverLabel.php',
+			'RolloverTab' => 		'Controls/Extended/RolloverTab.php',
+			'TabPanel' => 			'Controls/Extended/TabPanel.php',
+			'TransferPanel' => 		'Controls/Extended/TransferPanel.php',
+			'TreeList' => 			'Controls/Extended/TreeList.php',
+			'WindowPanel' => 		'Controls/Extended/WindowPanel.php',
+			
+			// Auxiliary
+			'AccordionPart' => 		'Controls/Auxilary/AccordionPart.php',
+			'ColumnHeader' => 		'Controls/Auxilary/ColumnHeader.php',
+			'MarkupItem' => 		'Controls/Auxilary/MarkupItem.php',
+			'Eventee' => 			'Controls/Auxilary/Eventee.php',
+			'Larva' => 				'Controls/Auxilary/Larva.php',
+			'ListViewItem' => 		'Controls/Auxilary/ListViewItem.php',
+			'MenuItem' => 			'Controls/Auxilary/MenuItem.php',
+			'Tab' => 				'Controls/Auxilary/Tab.php',
+			'TableColumn' => 		'Controls/Auxilary/TableColumn.php',
+			'TableRow' => 			'Controls/Auxilary/TableRow.php',
+			'TabPage' => 			'Controls/Auxilary/TabPage.php',
+			'TreeNode' => 			'Controls/Auxilary/TreeNode.php',
+			
+			// Statics
+			'Animate' => 			'Statics/Animate.php',
+			'ClientScript' => 		'Statics/ClientScript.php',
+			'Cursor' => 			'Statics/Cursor.php',
+			'Layout' => 			'Statics/Layout.php',
+			'Shift' => 				'Statics/Shift.php'
+		);
+	
+	if(isset($_NAutoLoad[$class]))
+		require($_NPath . $_NAutoLoad[$class]);
+	elseif(function_exists('__autoload'))
+		__autoload($class);
+}
+
+spl_autoload_register('_NAutoLoad');
 
 ?>
