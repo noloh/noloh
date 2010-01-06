7e08e92246b34052913c695715b35b328b8efc6d
Philip Ross <pross@noloh.com>
2010-01-06 00:12:15 +0000

    CollapsePanel Fixes Relating to Collapsed
     {
      issues:
      [
      {
      title: 'CollapePanel Fixes Relating to Collapsed',
      desc: 'Modified CollapsePanel to explicitly change Height to top, instead of animating. This address problems with ShiftWith.',
      internal: false
      }
      ],
      pullable: true,
      stable: true
      }

--- JavaScript/CollapsePanel.js
+++ JavaScript/CollapsePanel.js
@@ -16,15 +16,21 @@ function _NClpsPnlTgl(id, clpse)
 			body.style.display = '';
 			pnl.NullHgt = body.offsetHeight + _N(pnl.Top).offsetHeight;
 		}
-		new _NAni(id, "style.height", ((pnl.Hgt)?pnl.Hgt:pnl.NullHgt), 500);
+		new _NAni(id, "style.height", pnl.Hgt?pnl.Hgt:pnl.NullHgt, 500);
 		new _NAni(pnl.Body, "opacity", 100, 500);
 		pnl.Opn = true;
 	}
 	else if(pnl.Opn != false || clpse)
 	{
 		pnl.style.minHeight = _N(pnl.Top).offsetHeight + 'px';
-		var time = (pnl.InitClpse)?(pnl.InitClpse = 0):500;
-		new _NAni(id, "style.height", _N(pnl.Top).offsetHeight, time);
+		var time = 500;
+		if (pnl.InitClpse) 
+		{
+			time = pnl.InitClpse = 0; 
+			_NSetProperty(id, 'style.height', pnl.style.minHeight);
+		}
+		else 
+			new _NAni(id, "style.height", _N(pnl.Top).offsetHeight, time);
 		new _NAni(pnl.Body, "opacity", 'Hiding', time);
 		pnl.Opn = false;
 	}
