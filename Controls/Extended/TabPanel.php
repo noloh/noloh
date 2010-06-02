9f0e8924b58a1a13e7491c794e9b972befe4db57
Asher Snyder <asnyder@noloh.com>
2010-06-02 01:35:18 +0000

    TabPanel TabScrolling
    {"pullable":true,"stable":true,"issues":[{"feature":true,"title":"TabPanel TabScrolling","desc":"TabPanel now has a TabScrolling property that allows for a your tabs to scroll via Back and Next Buttons. TabPanel now has a TabScroller object which you can set the TabScroller->Next|Back|ScrollDuration|ScrollIncrement properties. Detailed article coming soon.","secret":"","order":"","internal":false}]}

--- Controls/Extended/TabPanel.php
+++ Controls/Extended/TabPanel.php
@@ -37,8 +37,14 @@ class TabPanel extends Panel
 	 */
 	public $TabPages;
 	private $Tabs;
+	private $TabScrolling;
+	private $TabScroller;
+	private $ScrollerInfo;
 
 	private $TabAlignment = 'Top';
+	//InnerSugar InnerClasses
+	static $_InTabScroller = 'HandleTabScroller';
+	
 	/**
 	 * Constructor
 	 * @param integer $left The Left coordinate of this element
@@ -52,29 +58,58 @@ class TabPanel extends Panel
 		$this->Tabs = new Group();
 		$this->Tabs->Change = new ClientEvent('_NTbPgSt', $this->Tabs->Id);
 
-		$this->TabBar = new Panel(0, 0, '100%', 0);
-		$this->Body = new Panel(0, 0, '100%', null, $this);
+		$this->TabScroller = new Panel(0, 0, '100%', 0);
+		$this->TabBar = new Panel(0, 0, '100%', '100%');
+		$this->Body = new Panel(0, 0, null, null, $this);
+//		$this->Body = new Panel(0, 0, '100%', null, $this);
 
 		$this->SetWidth($width);
 		$this->SetHeight($height);
 
-		$this->Body->Shifts[] = Shift::With($this, Shift::Height);
+		$this->Body->Shifts[] = Shift::SizeWith($this);
 
 		$this->TabPages = &$this->Body->Controls;
 		$this->TabPages->AddFunctionName = 'AddTabPage';
+		$this->TabPages->RemoveFunctionName = 'RemoveTabPage';
 
-		$this->TabBar->Layout = $this->Body->Layout = Layout::Relative;
-		$this->Controls->Add($this->TabBar);
-		$this->Controls->Add($this->Body);
+		$this->TabScroller->Layout = $this->Body->Layout = Layout::Relative;
+		$this->Controls->AddRange($this->TabScroller, $this->Body);
+	//	$this->Controls->Add($this->Body);
+		$this->TabScroller->Controls->Add($this->TabBar);
 		$this->TabBar->Controls->Add($this->Tabs);
 	}
+	/**
+	* Gets the TabScoller Panel which contains the TabScroller arrows, and TabBar
+	 */
+	public function GetTabScroller()	{return $this->TabScroller;}
 	/**
 	 * @ignore
 	 */
 	public function SetHeight($height)
 	{
 		parent::SetHeight($height);
-		$this->Body->SetHeight($height-$this->TabBar->GetHeight());
+		$this->Body->SetHeight($height - $this->TabScroller->GetHeight());
+	}
+	/**
+	* @ignore
+	*/
+	public function SetWidth($width)
+	{
+		parent::SetWidth($width);
+		$this->Body->SetWidth($width);
+		if(isset($this->ScrollerInfo))
+		{
+			if($this->TabScrolling === true)
+			{
+				if(isset($this->ScrollerInfo['back']))
+					$width -= $this->ScrollerInfo['back']->GetWidth();
+				if(isset($this->ScrollerInfo['next']))
+					$width -= $this->ScrollerInfo['next']->GetWidth();
+			}
+			$this->TabScroller->SetWidth($width);
+			if(!isset($this->TabScroller->Shifts['width']))
+				$this->TabScroller->Shifts['width'] =  Shift::WidthWith($this);
+		}
 	}
 	/**
 	 * Returns index of the currently selected TabPage
@@ -137,24 +172,147 @@ class TabPanel extends Panel
 			$tabPage = new TabPage($tabPage);
 
 		$rolloverTab = $tabPage->GetRolloverTab();
+		$rolloverTab->Leave[] = new ClientEvent('_NTbPgRm', $rolloverTab);
 		$tabHeight = $rolloverTab->GetHeight();
 
-		if($tabHeight > $this->TabBar->GetHeight())
+		if($tabHeight > $this->TabScroller->GetHeight())
 		{
-			$this->TabBar->SetHeight($tabHeight);
+			$this->TabScroller->SetHeight($tabHeight);
 			$this->SetHeight($this->Height);
 		}
-		$rolloverTab->Layout = Layout::Relative;
+		$rolloverTab->SetLayout(Layout::Relative);
 		$rolloverTab->CSSFloat = 'left';
 
 		$count = $this->Tabs->Count();
 		$this->Tabs->Add($rolloverTab);
-		$this->Body->Controls->Add($tabPage, true);
+		$this->TabBar->Controls->Add($rolloverTab);
+//		ClientScript::Set($rolloverTab, 'TabPg', $tabPage);
 		NolohInternal::SetProperty('TabPg', $tabPage->Id, $rolloverTab);
 		if($count == 0)
 			$this->SetSelectedIndex(0);
 		else
 			$tabPage->Visible = System::Vacuous;
+		
+		$tabScrolling = $this->TabScrolling;
+		if($tabScrolling === System::Auto || $tabScrolling === true)
+		{
+			if($tabScrolling === System::Auto)
+			{
+				ClientScript::Queue($this, '_NTbPgScrlChk', $this->TabBar);
+				/*Temporary Hack for checking scrollbars, won't work if auto is
+				set after adding tabs*/ 
+				$rolloverTab->Leave['scroller'] = new ClientEvent('_NTbPgScrlChk', $this->TabBar);
+			}
+			ClientScript::Queue($this, '_NTbPgScrl', array($this->TabBar, 'style.left', -1));
+		}
+			
+		return $this->Body->Controls->Add($tabPage, true);
+	}
+	function RemoveTabPage($tabPage)
+	{
+		$tabPage->GetRolloverTab()->Leave();
+		$this->Body->Controls->Remove($tabPage, true);
+	}
+	function HandleTabScroller()
+	{
+		$args = func_get_args();
+		switch($prop = strtolower(InnerSugar::$Tail))
+		{
+			case 'back':
+			case 'next':
+				$this->SetScroller($prop, $args[0]);
+				break;
+			case 'scrollincrement':
+			case 'scrollduration':
+				$this->ScrollerInfo[$prop] = $args[0];
+				ClientScript::Set($this->TabScroller, $prop, $args[0], '_N');
+				break;
+			default: throw new SugarException();
+		}
+	}
+	/**
+	* Gets whether the TabScroller Tabs scroll. True always displays TabScroller arrows, while System::Auto will automatically decide.
+	* @return true|false|System::Auto
+	*/
+	function GetTabScrolling()
+	{
+		return $this->TabScrolling !== null?$this->TabScrolling:false;
+	}
+	private function SetScrollerClient($prop, $value)
+	{
+		ClientScript::Set($this->TabScroller, $prop, $value, '_N');
+		$this->ScrollerInfo[$prop] = $value;
+	}
+	private function SetScroller($type, $object, $bypass=false)
+	{
+		if(isset($this->ScrollerInfo[$type]))
+			$this->Controls->Remove($this->ScrollerInfo[$type]);
+			
+		if(!$object)
+			$object =  new Button($type == 'back'?'<':'>', 0, 0, 20, $this->TabScroller->GetHeight());
+				
+		if($type == 'back')
+		{
+			$this->Controls->PositionalInsert($this->ScrollerInfo['back'] = $object, 0, 0)
+				->CasCSSFloat('left')
+				->CasLayout(Layout::Relative)
+//				->Click =  new ClientEvent("new _NAni('{$this->TabBar}', 'style.left', _N('{$this->TabBar}').offsetLeft + 100, 500);");
+				->Click =  new ClientEvent('_NTbPgScrl', $this->TabBar, 'style.left', 1);
+			ClientScript::Set($this->TabScroller, 'back', $this->ScrollerInfo['back'], '_N');
+			if($this->TabScrolling !== true)
+				$this->ScrollerInfo['back']->Visible = System::Vacuous;
+			
+		}
+		elseif($type == 'next')
+		{
+			$this->Controls->Add($this->ScrollerInfo['next'] = $object)
+//				->Click =  new ClientEvent("new _NAni('{$this->TabBar}', 'style.left', _N('{$this->TabBar}').offsetLeft - 100, 500);");
+				->Click =  new ClientEvent('_NTbPgScrl', $this->TabBar, 'style.left', -1);
+			$this->ScrollerInfo['next']->ReflectAxis('x');
+			ClientScript::Set($this->TabScroller, 'next', $this->ScrollerInfo['next'], '_N');
+			if($this->TabScrolling !== true)
+				$this->ScrollerInfo['next']->Visible = System::Vacuous;
+		}
+		if(!$bypass)
+			$this->SetWidth($this->GetWidth());
+	}
+	/**
+	* Sets whether the TabScroller Tabs scroll. True always displays TabScroller arrows, while System::Auto will automatically decide.
+	* 
+	* @param true|false|System::Auto $scroll
+	*/
+	function SetTabScrolling($scroll)
+	{
+		$this->TabScrolling = $scroll;
+		if($scroll !== false)
+			$this->TabBar->SetWidth(System::Auto);
+		
+		if($scroll !== false)
+		{
+			if(!isset($this->ScrollerInfo['scrollincrement']))
+				$this->SetScrollerClient('scrollincrement', 200);
+			if(!isset($this->ScrollerInfo['scrollduration']))
+				$this->SetScrollerClient('scrollduration', 200);
+			if(!isset($this->ScrollerInfo['back']))
+				$this->SetScroller('back', null,  true);
+			elseif($scroll === true)
+				$this->ScrollerInfo['back']->SetVisible(true);
+			if(!isset($this->ScrollerInfo['next']))
+				$this->SetScroller('next', null, true);
+			elseif($scroll === true)
+				$this->ScrollerInfo['next']->SetVisible(true);
+			//Change into tie-in of central SetWidth
+			$this->SetWidth($this->Width);
+		}
+		else
+		{
+			if(isset($this->ScrollerInfo['back']))
+				$this->ScrollerInfo['back']->SetVisible(System::Vacuous);
+			if(isset($this->ScrollerInfo['next']))
+				$this->ScrollerInfo['next']->SetVisible(System::Vacuous);
+		}
+//		$scroll === System::Auto?'auto':(($scroll === true)?true:false)
+		ClientScript::Set($this->TabScroller, 'auto', $scroll === System::Auto, '_N');
 	}
 	/**
 	 * @ignore
@@ -200,8 +358,7 @@ class TabPanel extends Panel
 	 */
 	function Show()
 	{
-		AddNolohScriptSrc('TabPanel.js');
-		//$initialProperties .= $this->GetEventString(null);
+		ClientScript::AddNOLOHSource('TabPanel.js');
 		parent::Show();
 	}
 }
