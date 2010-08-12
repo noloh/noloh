0aad1a2f77096ec60382200308c720878df53864
Philip Ross <pross@noloh.com>
2010-08-12 22:26:55 +0000

    Semantics
    {"pullable":true,"stable":true,"issues":[{"bug":true,"title":"Semantics","desc":"A bug in Semantics whereby Heading2 
    did not work.","secret":"","order":"","internal":false}]}

--- Statics/Semantics.php
+++ Statics/Semantics.php
@@ -8,8 +8,7 @@ final class Semantics
 {
 	const Normal = null;
 	const Paragraph = 'P';
-//	const Heading = 'H';
-	const Heading = 'H2';
+	const Heading = 'H';
 	const Heading1 = 'H1';
 	const Heading2 = 'H2';
 	const Heading3 = 'H3';
