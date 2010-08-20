b7865dca2020a4e8dadaadab157e1ddd0a4540c2
Philip Ross <pross@noloh.com>
2010-08-20 19:37:12 +0000

    URL encodings
    {"pullable":true,"stable":true,"issues":[{"bug":true,"title":"Search engine rendering","desc":"The links for them are now encoded, which is what spiders prefer.","secret":"","order":"","internal":false}]}

--- Controls/Core/Link.php
+++ Controls/Core/Link.php
@@ -327,7 +327,7 @@ class Link extends Label
 	function SearchEngineShow()
 	{
 		if($this->Destination !== '#')
-			echo '<A href="', $this->Destination===null && $GLOBALS['_NURLTokenMode'] ? ($_SERVER['PHP_SELF'].'?'.$this->TokenString()) : $this->Destination, '">', $this->Control ? $this->Control->SearchEngineShow() : $this->Text, '</A>';
+			echo '<A href="', $this->Destination===null && $GLOBALS['_NURLTokenMode'] ? ($_SERVER['PHP_SELF'].'?'.htmlspecialchars($this->TokenString())) : htmlspecialchars($this->Destination), '">', $this->Control ? $this->Control->SearchEngineShow() : $this->Text, '</A>';
 		else 
 		{
 			parent::SearchEngineShow();
--- System/Application.php
+++ System/Application.php
@@ -543,7 +543,14 @@ final class Application extends Object
 			if($trails !== false && isset($trails[$tokenString]))
 				foreach($trails[$tokenString] as $key => $info)
 					if(time()-$info[1] < $expiration)
-						$tokenLinks .= '<LI><A href="' . str_replace('&', '%26', $key[0]=='?'?(System::FullAppPath().$key):$key) . '">' . $info[0] . '</A></LI> ';
+					{
+						$href = $key[0]=='?'?(System::FullAppPath().$key):$key;
+						$pos = strpos($href, '?');
+						if($pos !== false)
+							$href = substr($href, 0, $pos) . htmlspecialchars(substr($href, $pos));
+						$tokenLinks .= '<LI><A href="' . $href . '">' . $info[0] . '</A></LI> ';						
+//						$tokenLinks .= '<LI><A href="' . str_replace('&', '%26', $key[0]=='?'?(System::FullAppPath().$key):$key) . '">' . $info[0] . '</A></LI> ';						
+					}
 		}
 		NolohInternal::NonstandardShowQueues();
 		$this->WebPage->SearchEngineShow('<UL>'.$tokenLinks.'</UL>');
