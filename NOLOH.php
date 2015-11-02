b505bd580c392dc6616210c9497dd75c47dedfd6
Philip Ross <philip.ross@flowtrac.com>
2015-11-02 15:16:59 -0600

    Just changing the version to see if auto-encrypt hook will work.

--- NOLOH.php
+++ NOLOH.php
@@ -20,6 +20,6 @@ function ComputeNOLOHPath()	{return dirname(__FILE__);}
  * Gets the current version of NOLOH
  * @return string
  */
-function GetNOLOHVersion() {return '1.8.105';}   // git rev-list HEAD --count, +1.
+function GetNOLOHVersion() {return '1.8.108';}   // git rev-list HEAD --count, +1.
 
 ?>
