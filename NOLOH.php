90e298100d52b60de658c1aa52a85dc3117ea303
Flowtrac <no-reply@flowtrac.com>
2016-04-01 10:48:38 -0500

    beta-gzip
    updated core version

--- NOLOH.php
+++ NOLOH.php
@@ -20,6 +20,6 @@ function ComputeNOLOHPath()	{return dirname(__FILE__);}
  * Gets the current version of NOLOH
  * @return string
  */
-function GetNOLOHVersion() {return '1.8.177';}   // Run "git rev-list HEAD --count", then +1.
+function GetNOLOHVersion() {return '1.8.180';}   // Run "git rev-list HEAD --count", then +1.
 
 ?>
