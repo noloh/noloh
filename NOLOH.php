b19f101533ca7e061deebd580963f170df85ee19
Flowtrac <no-reply@flowtrac.com>
2017-08-30 14:39:08 -0500

    New menu look
    Updating Core Version
    
    Preventing fancy-panel or k-seperators from receiving a context menu link
    Ensuring custom context menu is applied regardless of rich markup

--- NOLOH.php
+++ NOLOH.php
@@ -20,5 +20,5 @@ function ComputeNOLOHPath()	{return dirname(__FILE__);}
  * Gets the current version of NOLOH
  * @return string
  */
-function GetNOLOHVersion() {return '1.8.222';}   // Run "git rev-list HEAD --count", then +1.
+function GetNOLOHVersion() {return '1.8.223';}   // Run "git rev-list HEAD --count", then +1.
 ?>
