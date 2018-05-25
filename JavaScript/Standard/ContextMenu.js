39fff5d4596f6b1b3eb0c3e92e2b36fdcab70035
Flowtrac <no-reply@flowtrac.com>
2018-05-25 16:19:49 -0500

    Fix right clicking menus in core
    Fix bug with right clicking in beta

--- JavaScript/Standard/ContextMenu.js
+++ JavaScript/Standard/ContextMenu.js
@@ -1,12 +1,13 @@
-function _NCMShow(obj)
+function _NCMShow(obj, e)
 {
+	var evt = event || e;
 	_NCMHide();
 	var id = _N.ContextMenu = obj.ContextMenu, contextMenu = _N(id);
 	contextMenu.style.display = "";
-	_NSet(id, "style.left", event.pageX + "px");
-	_NSet(id, "style.top", event.pageY + "px");
+	_NSet(id, "style.left", evt.pageX + "px");
+	_NSet(id, "style.top", evt.pageY + "px");
 	_N.EventVars.ContextMenuSource = obj.id;
-	_NClickOff(id, _NCMHide);
+	_NClickOff(id, _NCMHide, evt);
 	_NNoBubble();
 }
 function _NCMHide()
@@ -16,5 +17,6 @@ function _NCMHide()
 		if(!_N.SEQ.length)
 			delete _N.EventVars.ContextMenuSource;
 		delete _N.ContextMenu;
+		delete _N.ClickOffId;
 	}
 }
