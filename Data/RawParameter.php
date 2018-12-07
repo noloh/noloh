80d5f0ab7b002038786774c3d4edeec823167a28
Philip Ross <philip.ross@flowtrac.com>
2018-12-07 17:11:27 -0600

    PHP7.2 compatibility for RawParameter

--- Data/RawParameter.php
+++ Data/RawParameter.php
@@ -5,7 +5,7 @@
  * The Raw Parameter class is used to inject code into SQL statements that don't need to be parsed
  * such as SQL function calls or object names like tables, columns, databases, etc
  */
-class RawParameter extends Object
+class RawParameter extends Base
 {
 	protected $Parameter;
 	
