3bc0597fe5bb7d3f3cecfb113542960932f1496d
Flowtrac <no-reply@flowtrac.com>
2020-02-20 15:45:47 -0600

    disable buttons while in edit mode on all grids
    improved excel enable/disable logic

--- NOLOH.php
+++ NOLOH.php
@@ -20,5 +20,5 @@ function ComputeNOLOHPath()	{return dirname(__FILE__);}
  * Gets the current version of NOLOH
  * @return string
  */
-function GetNOLOHVersion() {return '1.8.1001';}   // Run "git rev-list HEAD --count", then +1.
+function GetNOLOHVersion() {return '1.8.1007';}   // Run "git rev-list HEAD --count", then +1.
 ?>
