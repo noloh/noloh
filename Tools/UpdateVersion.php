a51495328faa7f9ad88ab71ebee7029f2ccced9d
Flowtrac <no-reply@flowtrac.com>
2020-02-24 09:03:21 -0600

    disable buttons while in edit mode on all grids
    updated core version and version updating tool

--- Tools/UpdateVersion.php
+++ Tools/UpdateVersion.php
@@ -4,7 +4,7 @@ require_once('../NOLOH.php');
 
 chdir('..');
 
-$revCount = System::Execute('git rev-list HEAD --count');
+$revCount = System::Execute('git rev-list HEAD --count') % 1000;
 $revCount += 1;
 
 $fileContents = file_get_contents('NOLOH.php');
