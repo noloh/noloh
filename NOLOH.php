f8f2e48caf259de341bc3351b509e4db076144ee
Flowtrac <no-reply@flowtrac.com>
2020-04-02 11:26:18 -0500

    Limit the number of records user can print
    incrementing NOLOH version

--- NOLOH.php
+++ NOLOH.php
@@ -20,6 +20,6 @@ function ComputeNOLOHPath()	{return dirname(__FILE__);}
  * Gets the current version of NOLOH
  * @return string
  */
-function GetNOLOHVersion() {return '1.9.142';}   // Run "git rev-list HEAD --count" % 1000, then +1.
+function GetNOLOHVersion() {return '1.9.149';}   // Run "git rev-list HEAD --count" % 1000, then +1.
 
 ?>
