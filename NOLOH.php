536827ab5a56562151b7e438b49697b1c83444ad
Flowtrac <no-reply@flowtrac.com>
2020-11-30 16:42:38 -0600

    Unable to schedule Grid
    Modify grid state column comparison to take computed columns into account
    Modify GetFilteredData to include computed columns

--- NOLOH.php
+++ NOLOH.php
@@ -22,7 +22,7 @@ function ComputeNOLOHPath()	{return dirname(__FILE__);}
  */
 function GetNOLOHVersion()
 {
-	return '1.9.541'; // Run "git rev-list HEAD --count" % 1000, then +1.
+	return '1.9.547'; // Run "git rev-list HEAD --count" % 1000, then +1.
 }
 
 ?>
