0ee1a7052a49c869dd73ec18f0cb3cc487fc151d
Flowtrac <no-reply@flowtrac.com>
2020-12-04 11:46:25 -0600

    -core Integrate with Passbolt
    Created Passbolt class to handle login, resource creation, and resource sharing.
    Updated core version.

--- NOLOH.php
+++ NOLOH.php
@@ -22,7 +22,7 @@ function ComputeNOLOHPath()	{return dirname(__FILE__);}
  */
 function GetNOLOHVersion()
 {
-	return '1.9.557'; // Run "git rev-list HEAD --count" % 1000, then +1.
+	return '1.9.560'; // Run "git rev-list HEAD --count" % 1000, then +1.
 }
 
 ?>
