e9e7379687463a4575f62f96658b68804864b373
Flowtrac <no-reply@flowtrac.com>
2021-01-08 12:00:00 -0600

    Kendo Chart Numeric Formatting (SECRET)
    Increment version

--- NOLOH.php
+++ NOLOH.php
@@ -22,7 +22,7 @@ function ComputeNOLOHPath()	{return dirname(__FILE__);}
  */
 function GetNOLOHVersion()
 {
-	return '1.9.594'; // Run "git rev-list HEAD --count" % 1000, then +1.
+	return '1.9.609'; // Run "git rev-list HEAD --count" % 1000, then +1.
 }
 
 ?>
