2da55b4803be680b951adabbd7f29e1d37538e96
Flowtrac <no-reply@flowtrac.com>
2021-02-23 18:43:53 -0600

    Re-implement email functionality with a new library
    Add SwiftMailer based email class to core

--- Includes.php
+++ Includes.php
@@ -33,6 +33,7 @@ require($_NPath . 'Statics/Dir.php');
 if (version_compare(PHP_VERSION, '7.2.0') >= 0)
 {
 	require($_NPath . 'Nodules/Guzzle/5.3.3/autoload.php');
+	include($_NPath . 'vendor/autoload.php');
 }
 else
 {
@@ -155,6 +156,13 @@ function _NAutoLoad($class)
 			'Semantics' =>			'Statics/Semantics.php',
 			'Shift' => 				'Statics/Shift.php'
 		);
+
+		if (version_compare(PHP_VERSION, '7.2.0') >= 0)
+		{
+			$_NAutoLoad = array_merge($_NAutoLoad, array(
+				'SwiftEmailHelper' => 'Nodules/EmailHelper/SwiftEmailHelper.php'
+			));
+		}
 	}
 
 	$namespace = null;
